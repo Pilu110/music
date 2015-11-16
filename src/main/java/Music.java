0ce6ca337220b79408ae6821e58cb51f933e7b66
Istvan Vincze <pilu110@gmail.com>
2015-11-16 23:46:07 +0100

    Initial commit.

---
+++ src/main/java/Music.java
@@ -0,0 +1,9 @@
+import org.jfugue.player.Player;
+public class Music
+{
+    public static void main(String args[])
+    {
+        Player player = new Player();
+        player.play("V0 I[Piano] Eq Ch. | Eq Ch. | Dq Eq Dq Cq   V1 I[Flute] Rw | Rw | GmajQQQ CmajQ");
+    }
+}
